9b85182ee71c391e5251104f461cdab988a9563e
congo-quinine <congo.quinine@gmail.com>
2019-02-12 15:14:06 +0000

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,11 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink to="/" exact activeStyle={{ background: 'darkblue' , paddingRight: '10px' }}>Home</NavLink>
+      <NavLink to="/movies" exact activeStyle={{ background: 'darkblue' , paddingLeft: '10px' }}>Movies</NavLink>
+      <NavLink to="/directors" exact activeStyle={{ background: 'darkblue', paddingRight: '10px'}}>Directors</NavLink>
+      <NavLink to="/actors" exact activeStyle={{ background: 'darkblue', paddingRight: '10px' }}>Actors</NavLink>
     </div>
   );
 };
