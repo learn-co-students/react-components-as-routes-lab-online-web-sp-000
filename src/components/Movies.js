ec54ce52078c64f600290b68340cd3296b1eb708
tuzmusic <tuzmusic@gmail.com>
2019-03-13 15:13:57 -0400

    render movie page (on screen)

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      <div id="movies-list">
+        {movies.map((movie, i) => (
+          <Movie key={i.toString()} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Movies;
+
+const Movie = props => {
+  return (
+    <div id="movie">
+      <h2>Name: {props.movie.title}</h2>
+      <p>Time: {props.movie.time}</p>
+      <p>Genres:</p>
+      <ul>
+        {props.movie.genres.map(genre => (
+          <li>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = props => (
-  <ul>
-    {props.routes.map((page,i ) => (
-      <li>
-        <NavLink key={i} to={`/${page === "home" ? '' : page}`} exact>
-          {page[0].toUpperCase() + page.slice(1)}
-        </NavLink>
-      </li>
-    ))}
-  </ul>
+  <div class="navBar">
+    <ul>
+      {props.routes.map((page, i) => (
+        <li key={i.toString()}>
+          <NavLink to={`/${page === "home" ? "" : page}`} exact>
+            {page[0].toUpperCase() + page.slice(1)}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
 
 export default NavBar;
