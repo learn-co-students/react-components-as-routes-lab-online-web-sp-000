7a23619d4340f6bbb8805debb04b07f60a0dfa0a
tuzmusic <tuzmusic@gmail.com>
2019-03-13 11:48:11 -0400

    add links using NavLink

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+const NavBar = props => (
+  <ul>
+    {props.routes.map((page,i ) => (
+      <li>
+        <NavLink key={i} to={`/${page === "home" ? '' : page}`} exact>
+          {page[0].toUpperCase() + page.slice(1)}
+        </NavLink>
+      </li>
+    ))}
+  </ul>
+);
 
 export default NavBar;
