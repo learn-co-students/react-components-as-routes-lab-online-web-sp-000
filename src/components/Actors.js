4c26b0ef57873c2eb7db4dbfdcd8b8b60c446527
Cheng0315 <csalvue@gmail.com>
2019-04-03 00:20:17 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, idx) => (
+        <div key={idx}>
+          <h3>Name: {actor.name}</h3>
+          <ul>
+            Movies:
+            <li>{actor.movies[0]}</li>
+            <li>{actor.movies[1]}</li>
+            <li>{actor.movies[2]}</li>
+          </ul>
+        </div>
+    ))};
     </div>
   );
 };
