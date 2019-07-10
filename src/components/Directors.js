c840e40711a5f202196e36f8bbdee273d0f35c13
alexmpalermo <alexmpalermo@gmail.com>
2019-07-09 21:47:08 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,19 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(director => (
+      <div key={director.name}>
+        {director.name}
+        <ul>
+        {director.movies.map(movie => (
+          <li key={movie}>{movie}</li>
+        ))}
+        </ul>
+      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Directors
