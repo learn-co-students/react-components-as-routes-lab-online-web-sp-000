01fe3c0951ec958a1057cea240f5cc5add90263d
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-25 21:42:27 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,11 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) =>
+        <div>{movie.title} {movie.time}
+          <ul>{movie.genres.map((genre) => (<li>{genre}</li>))}</ul>
+        </div>)}
     </div>
   );
 };
