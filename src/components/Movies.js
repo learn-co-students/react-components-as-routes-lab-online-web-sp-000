348baee7a93e09750a0d857f9a0a465202f1e8ac
fut0k0 <cccabell@icloud.com>
2019-10-22 10:41:00 -0700

    Passing lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,42 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
+
+const listGenres = (genres) => {
+
+	return (
+		<ul>
+			{genres.map(genre => <li key={genre}>{genre}</li>)}
+		</ul>
+	)
+
+}
+
+const displayMovies = () => {
+
+	return (
+		movies.map(movie => {
+			return (
+				<div key={movie.title}>
+					<h3>Name: {movie.title}</h3>
+					<span>Time: {movie.time}</span>
+					<br/><br/>
+					<span>Genres: {listGenres(movie.genres)}</span>
+				</div>
+			);
+		})
+	)
+
+}
 
 const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
-};
 
-export default Movies;
+	return (
+		<div>
+			<h1>Movies Page</h1>
+			{displayMovies()}
+		</div>
+	)
+
+}
+
+export default Movies
