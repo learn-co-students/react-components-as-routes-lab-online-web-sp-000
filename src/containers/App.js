89a0e7f1c10caaf510f0c2d23e4b7e0a89c9b382
John Hayes <JohnnyHayes7@gmail.com>
2019-12-13 12:00:38 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,13 +15,14 @@ const App = (props) => {
     <Router>
       <div>
         < NavBar />
+       
         < Route exact path="/" component={Home} />
           
-        <Route exact path="/movies" component={Movies} />
+        <Route path="/movies" component={Movies} />
          
-        <Route exact path="/directors" component={Directors} />
-          
-        <Route exact path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
+         
+        <Route path="/actors" component={Actors} />
           
       </div>
     </Router>
