9d20b71c9056b0a3b5c40b4c16ebfe90e7ad6d31
davisjustinw <davisjustinw@gmail.com>
2020-01-27 21:51:42 -0800

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Switch,
   Route
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
@@ -13,7 +14,23 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar/>
+        <Switch>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/directors">
+            <Directors />
+          </Route>
+          <Route path="/actors">
+            <Actors />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 };
