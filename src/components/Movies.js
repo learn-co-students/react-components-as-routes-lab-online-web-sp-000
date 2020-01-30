5824bc95a47d1616422d23f5be0db5ecfeb68f9f
Tsunamins <reillyamr@gmail.com>
2020-01-30 12:59:22 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>{movie.title} {movie.time}</h3>
+          {movie.genres.map((genre, index) =>(
+            <ul>
+              <li key={index}>{genre}</li>
+            </ul>
+          ))}
+          </div>
+          ))}
     </div>
   );
 };
