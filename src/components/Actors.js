c293bd83e7a2650115765633a17802b96a51ceff
ch2isk4kos <ch2isk4kos@gmail.com>
2020-03-02 14:37:19 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,11 +2,51 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
-  return (
+    return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        <hr></hr>
+        {actors.map((actor, index) => (     // why ()'s and not {}'s ???
+            <div key={index} className="actor">
+                <h3>{actor.name}</h3>
+
+                <p>Movies:</p>
+                <ul>
+                {actor.movies.map((movie, index) => (   // why ()'s and not {}'s ???
+                    <li key={index}>{movie}</li>
+                ))}
+                </ul>
+
+                <hr></hr>
+            </div>
+        ))}
     </div>
-  );
+    );
 };
 
 export default Actors;
+
+// 1st attempt:
+
+// const Actors = () => {
+//     return (
+//     <div>
+//         <h1>Actors Page</h1>
+//
+//         {actors.map((actor, index) => {  // why do I need to add ()'s instead of {}'s after the arrow function?
+//             <div className="actor">
+//                 <h3>{actor.name}</h3>
+//                 <h3>Movies:</h3>
+//                 <ul>
+//                     {actor.movies.map((movie, index) => {
+//                         <li key={index}>{movie}</li> })
+//                     }
+//                 </ul>
+//             </div>
+//             })
+//         }
+//     </div>
+//     );
+// };
+//
+// export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,31 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <div>
-      {/*{code here}*/}
+    return (
+    <div className="navbar">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
     </div>
   );
 };
 
 export default NavBar;
+
+// <nav>
+//   <ul>
+//     <li>
+//       <Link to="/">Home</Link>
+//     </li>
+//     <li>
+//       <Link to="/about">Actors</Link>
+//     </li>
+//     <li>
+//       <Link to="/users">Directors</Link>
+//     </li>
+//     <li>
+//       <Link to="/users">Movies</Link>
+//     </li>
+//   </ul>
+// </nav>
