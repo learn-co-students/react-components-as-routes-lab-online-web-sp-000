6f119970807d64d25208e4f15c9046669d31cbca
ericabasak <ericabasak@gmail.com>
2020-03-11 21:32:09 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,16 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+        return(
+          <div>
+            <h3>Name: { actor.name }</h3>
+            <h4>{actor.movies.map(movie => <ul>{ movie }</ul>)}</h4>
+          </div>
+        )
+      }
+      )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,19 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div> 
+      <h1>Movies Page</h1>
+        <h3>{movies.map(movie => {
+            return(
+              <div>
+                <h3>Movie Title: { movie.title }</h3>
+                <h3>Time: { movie.time }</h3>
+                <h4>Genre: { movie.genres.map(genre => <ul>{ genre }</ul>)}</h4>
+              </div>
+            )
+          }
+          )}
+        </h3>
     </div>
   );
 };
