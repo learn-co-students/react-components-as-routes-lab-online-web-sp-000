8a339ce2d90b40efbf67d7296699bd6822fdaf0c
morganpstanley <morganpstanley@gmail.com>
2020-03-17 00:47:36 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+         <h1>Movies Page</h1>
+
+         {movies.map((movie) => (
+          <div>
+          <h2>{movie.title}</h2>
+          <h2>{movie.time}</h2>
+          <h2>{movie.genres}</h2>
+          </div>
+         ))}
     </div>
   );
 };
