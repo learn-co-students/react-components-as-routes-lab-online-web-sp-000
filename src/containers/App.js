e3b31ad643ee24e811de46520f28898a14188685
freddysilber <freddy.silber@cloudcoach.com>
2020-04-14 16:04:11 -0600

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Home from '../components/Home';
-import Actors from '../components/Actors';
-import Directors from '../components/Directors';
-import Movies from '../components/Movies';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import NavBar from '../components/NavBar'
+import Home from '../components/Home'
+import Actors from '../components/Actors'
+import Directors from '../components/Directors'
+import Movies from '../components/Movies'
 
 
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div className="app">
+        <NavBar />
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/actors" component={Actors} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/movies" component={Movies} />
+      </div>
     </Router>
-  );
-};
+  )
+}
 
 export default App
