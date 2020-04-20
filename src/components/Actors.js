e1181c29774a30c98d90c4cb3b6f5c35a4bc5853
oliverpdahl <oliverpdahl@gmail.com>
2020-04-20 12:46:41 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,25 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(Actor)}
     </div>
   );
 };
+
+const Actor = ({ name, movies }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      {movies.map(title => Movie({title: title}))}
+    </div>
+  )
+}
+
+const Movie = ({ title }) => {
+  return ( 
+    <p>{title}</p>
+  )
+}
 
 export default Actors;
