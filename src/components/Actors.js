0bb6f81beafbe5f590856ed6569b17bfee3fb2aa
BlythePollard <github email address>
2020-04-22 17:04:14 -0600

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,10 +4,22 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
-};
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          {actor.movies.map((movie, index) => (
+            <li key={index}>{movie}</li>
+          ))}
+
+        </div>
+      ))}
+       </div>
+  )
+}
+
+
+
 
 export default Actors;
 
