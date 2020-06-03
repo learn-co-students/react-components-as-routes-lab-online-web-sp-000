61b13dfdb4f1c0311ed28caa0a2319b6f7fdabd2
Kristinamarih <kristinahorensky@gmail.com>
2020-06-03 13:57:52 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>'Actors Page'</h1>
+      {actors.map(actor => (
+        <div>
+          <h3 classname="actor">Name: {actor.name}</h3>
+          <p>Movies:</p>
+          {actor.movies.map(movie => <ul>{movie}</ul>)}
+        </div>
+      ))}
     </div>
   );
 };
