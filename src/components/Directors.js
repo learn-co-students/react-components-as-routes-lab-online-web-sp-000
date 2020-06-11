50c3c251d337c657b019bf2c5a544a88b34a609c
bfarberman <bfarberman@gmail.com>
2020-06-11 07:45:33 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,20 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {
+          directors.map((director, index) => (
+            <div>
+              <h1>{director.name}</h1>
+              
+              <ul>
+                {director.movies.map((movie, index) => (
+                  <li>{movie}</li>
+                ))}
+              </ul>
+            </div>
+          ))
+        }
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {
+          movies.map((movie, index) => (
+            <div>
+              <h1>{movie.title}</h1>
+              <h1>{movie.time}</h1>
+              <ul>
+                {movie.genres.map((genre, index) => (
+                  <li>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))
+        }
     </div>
   );
 };
