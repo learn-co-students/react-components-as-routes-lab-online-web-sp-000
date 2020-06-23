11f1942278961a52c9cf792ae11779420b2822ce
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-22 20:25:22 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,17 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div className="movie">
+          <h3>Title: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => (
+              <li>{genre}</li>
+            ))}
+          </ul>
+        </div>))}
     </div>
   );
 };
