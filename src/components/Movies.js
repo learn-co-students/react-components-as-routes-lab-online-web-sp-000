6c10d6ae02d90f21b51a96c8698c32ba4f2d886d
azbury <acasbury25@aol.com>
2020-07-02 10:47:07 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,38 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        <div className="movie">
+        <h2>{movies[0].title}</h2>
+        <h2>{movies[0].time}</h2>
+        <ul>
+          <li>{movies[0].genres[0]}</li>
+          <li>{movies[0].genres[1]}</li>
+          <li>{movies[0].genres[2]}</li>
+        </ul>
+      </div>
+      <div className="movie">
+        <h2>{movies[1].title}</h2>
+        <h2>{movies[1].time}</h2>
+        <ul>
+          <li>{movies[1].genres[0]}</li>
+          <li>{movies[1].genres[1]}</li>
+          <li>{movies[1].genres[2]}</li>
+          <li>{movies[1].genres[3]}</li>\
+          <li>{movies[1].genres[4]}</li>
+        </ul>
+      </div>
+      <div className="movie">
+        <h2>{movies[2].title}</h2>
+        <h2>{movies[2].time}</h2>
+        <ul>
+          <li>{movies[2].genres[0]}</li>
+          <li>{movies[2].genres[1]}</li>
+          <li>{movies[2].genres[2]}</li>
+          <li>{movies[2].genres[3]}</li>
+          <li>{movies[2].genres[4]}</li>
+        </ul>
+      </div>
     </div>
   );
 };
