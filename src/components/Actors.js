2e8417d3f5dddc078307683de841d5a3702d418a
rehmanali001 <rehmanali001@gmail.com>
2020-07-08 21:16:47 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,9 +4,17 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+      <h1>Actors Page</h1>
+      {renderActors()}    </div>
   );
 };
+
+function renderActors(){
+  return actors.map(actor => <div>{actor.name}<ul>{renderMovies(actor.movies)}</ul></div>)
+}
+
+function renderMovies(movies){
+  return movies.map(movie => <li>{movie}</li>);
+}
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,23 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+	      <nav className="navbar">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/directors">Directors</NavLink>
+          </li>
+          <li>
+            <NavLink to="/actors">Actors</NavLink>
+          </li>
+        </ul>
+      </nav>    
+      </div>
   );
 };
 
