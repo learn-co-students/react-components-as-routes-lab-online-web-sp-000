fa90fb1d3301aa7787897a46b2c2a9cac4c42e92
Soner Mezgitci <mezgitci9@gmail.com>
2020-08-19 11:36:49 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { actors } from '../data';
+import Movies from './Movies';
 
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
-  );
+      <h1> Actors Page</h1>
+        {actors.map(actor => (
+        <div>
+       <ul>
+        <li>{actor.name}</li>
+       </ul>
+       <ul>
+       {actor.movies.map((movie, index) => (
+      <li key={index}>{movie}</li>
+       ))}
+       </ul>
+   
+        </div>
+   ))}
+      </div>
+  )
 };
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { movies } from '../data';
 
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
-  );
+      <h1> Movies Page</h1>
+       {movies.map( movie => (
+        <div>
+        <ul>
+          <li>{movie.title}</li>
+          <li>{movie.time}</li>
+        </ul>
+        <ul>
+        {movie.genres.map((genre, index) => ( 
+        
+          <li key={index}>{genre}</li>
+       ))}
+        </ul>
+       
+        </div>
+       ))}
+      </div>
+       
+  )
 };
 
 export default Movies;
