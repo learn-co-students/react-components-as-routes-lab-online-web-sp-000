2820edc150f050720683aef9079b5971b3368d32
doakcovington <doakcovington@protonmail.com>
2020-08-20 14:46:46 -0700

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <h3>{movie.title}</h3>
+            <p>{movie.time}</p>
+            {movie.genres.map((genre, index) => (
+              <ul key={index}>
+                <li>{genre}</li>
+              </ul>
+            ))}
+          </div>
+        ))}
     </div>
   );
 };
