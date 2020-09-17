6a6ccec1850c858c205dd5228d4191de4caa8ba6
Anthonyntilelli <Anthony@tilelli.me>
2020-09-17 13:05:07 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ const NavBar = () => {
   return (
     <header>
       <div className="navbar">
-        <NavLink to="/" activeClassName="is-active" exact={true}>Home </NavLink>
-        <NavLink to="/movies" activeClassName="is-active">Movies </NavLink>
-        <NavLink to="/directors" activeClassName="is-active">Directors </NavLink>
-        <NavLink to="/actors" activeClassName="is-active">Actors </NavLink>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+        <NavLink to="/directors">Directors</NavLink>
+        <NavLink to="/actors">Actors</NavLink>
       </div>
     </header>
   );
