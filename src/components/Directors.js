b6380b9b200c4c12571ac47b254c311570c8c7cd
Christopher Dent <christopherdent01@gmail.com>
2020-11-04 21:21:45 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { directors } from '../data';
 
+
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+      <h1>Directors Page</h1>
+
+      {directors.map( director => (
+        <div>
+          {director.name}
+          <ul>
+            {director.movies.map(movie => (
+              <li>{movie}</li>
+            ))}
+          </ul>
+      </div>
+    ))};
+  </div>
   );
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map( movie => (
+          <div>
+        Movie Name: {movie.title}<br />
+      Running Time:  {movie.time} <br />
+    <ul>Genres{movie.genres.map(genre =>
+        (<li>{genre}</li>)
+        )}
+
+    </ul>
+          </div>
+        ))}
     </div>
   );
 };
