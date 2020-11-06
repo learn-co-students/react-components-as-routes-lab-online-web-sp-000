5082de1c11e42dee50be0d96b03a0be5fed89fb9
Elliot Blanchard <git@elliotblanchard.com>
2020-11-05 21:10:05 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { movies } from '../data';
 
+const Movie = ({
+  title,
+  time,
+  genres
+}) => {
+  return (
+    <div>
+      <h2>Name: {title}</h2>
+      <p>Time: {time}</p>
+      <p>Genres: </p>
+      <ul>
+        {genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+        ))} 
+      </ul>
+    </div>
+  );
+};
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        {<h1>Movies Page</h1>}
+        {movies.map(Movie)}
     </div>
   );
 };
