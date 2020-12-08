b51357f528d1806701f16f48dbf8e7f20f054a03
blumenthaler <blumenthaleralex@gmail.com>
2020-12-08 14:20:18 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        {<h1>Movies Page</h1>}
+        {
+          movies.map(movie => {
+            return (
+              <div>
+                <h2>Title: {movie.title}</h2>
+                <h3>Runtime: {movie.time}</h3>
+                <h3>Genres: </h3>
+                <ul>
+                  {movie.genres.map(genre => <li>{genre}</li>)}
+                </ul>
+              </div>
+            )
+          })
+        }
     </div>
   );
 };
