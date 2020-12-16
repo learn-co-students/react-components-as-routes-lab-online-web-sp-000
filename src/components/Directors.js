8ced83aa800c755ad210b090fe6cda46e7d653e1
Dwaynew85 <mr_dwayne_w@yahoo.com>
2020-12-16 12:15:32 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,15 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+        {directors.map(director => (
+          <div>
+            <h2 key={directors.indexOf(director.name)}>{director.name}</h2>
+            <ul>
+              {director.movies.map(movie => ( <li key={director.movies.indexOf(movie)}>{movie}</li> ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            <h2 key={movies.indexOf(movie.title)}>{movie.title}</h2>
+            <h3>Time: {movie.time}</h3>
+            <ul>
+              {movie.genres.map(genre => ( <li key={movie.genres.indexOf(genre)}>{genre}</li> ))}
+            </ul>
+          </div>
+        ))}
     </div>
   );
 };
