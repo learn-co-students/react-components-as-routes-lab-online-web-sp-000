a228cfee89b6cd2e2da192d24bbe3f2c78950f7e
carrollm2 <carrollm2@gmail.com>
2021-01-12 23:14:50 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,19 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, id) =>(
+          <div key={id}>
+            <h3>Name: {movie.title}</h3>
+            <p>Time: {movie.time} minutes</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, id) =>
+                <li key={id}>{genre} </li>
+              )}
+            </ul>
+          </div>
+        ))}        
     </div>
   );
 };
