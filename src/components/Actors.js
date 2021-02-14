37f3e354bb4be5cd5d4e2ee9da5fedd8cc9ce197
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-02-13 18:54:35 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { actors } from '../data';
 
+const showActors = () => {
+  return (
+    actors.map( actor => {
+      return (
+        <div>
+          <h3>Name: {actor.name}</h3>
+          <ul>{showMovies(actor.movies)}</ul>
+        </div>
+      )
+    })
+  )
+}
+
+const showMovies = (movies) => {
+  return movies.map( movie => <li>{movie}</li>)
+}
+
 const Actors = () => {
   return (
-    <div>
-      {/*{code here}*/}
-    </div>
+    <React.Fragment>
+      <h1>Actors Page</h1>
+      {showActors()}
+    </React.Fragment>
   );
 };
 
