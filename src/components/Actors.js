d2ee9e31e2854b525ffd7b97fd4d97a6151fb56b
CoderJay06 <jaycru06@yahoo.com>
2021-03-07 03:26:15 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor => {
+         return (
+            <div key={actor.name}>
+               <h3>{actor.name}</h3>
+               <h4>Movies:</h4>
+               <ul>
+                  {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+               </ul>
+            </div>
+         )
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,29 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+            return (
+            <div key={movie.title}>
+               <h3>{movie.title}</h3>
+               <time>Time: {movie.time}</time>
+               <h4>Genres:</h4>
+               <ul className="genres">
+                  {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+               </ul>
+            </div>)
+         })}
     </div>
   );
 };
+
+// const renderMovies = () => {
+//    return movies.map(movie => {
+//       return (
+//       <div key={movie.title}>
+//          <h3>{movie.title}</h3>
+//       </div>)
+//    })
+// }
 
 export default Movies;
