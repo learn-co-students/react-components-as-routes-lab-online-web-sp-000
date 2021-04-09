9663557013a629b77ff7f497eae10cc7f5260428
khoinguyenkc <khoinguyenkc@gmail.com>
2021-04-08 17:54:12 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map( actor => { return (<div>
+        <p>{actor.name}</p>
+        <p>Movies:</p>
+        <ul>
+          {actor.movies.map( movie => { return <li>{movie}</li>})}
+        </ul>
+      </div>)})}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+          {movies.map( movie => { return (<div>
+            <p>Name: {movie.title}</p>
+            <p>Title: {movie.time}</p>
+            <p>Genres:</p>
+              <ul>
+                {movie.genres.map( genre => <li>{genre}</li>)}
+
+              </ul>
+
+
+
+
+
+          </div>)}
+        )}
+
     </div>
   );
 };
