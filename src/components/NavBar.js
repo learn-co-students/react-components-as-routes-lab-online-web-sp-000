ddbec22df90010cd7f017820326baa9f54b0e0c7
RyanBegnaud <Ryanbegnaud13@gmail.com>
2021-04-11 21:44:03 -0700

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,12 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <ul>
+      <NavLink to ='/'>Home</NavLink><br></br>
+      <NavLink to ='/movies'>Movies</NavLink><br></br>
+      <NavLink to ='/directors'>Directors</NavLink><br></br>
+      <NavLink to ='/actors'>Actors</NavLink>
+      </ul>
     </div>
   );
 };
