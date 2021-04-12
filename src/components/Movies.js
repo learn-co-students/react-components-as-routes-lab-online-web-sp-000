0e44cedce307fb3055027cefffed8b70b298b7cc
Robert Clarke <r.jason.clarke@gmail.com>
2021-04-12 20:12:11 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (makeMovie(movie)))}
     </div>
   );
 };
+
+const makeMovie = (movie) => {
+  return(
+    <div>
+      <h2>{movie.title}</h2>
+      <h3>{movie.time}</h3>
+      <ul>{movie.genres}</ul>
+    </div>
+  )
+}
 
 export default Movies;
