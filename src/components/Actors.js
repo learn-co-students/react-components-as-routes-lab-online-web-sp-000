f6054db81f4fa93efbf8993d64c522a9d56f15d5
RSigmaSG <sg.comparch@gmail.com>
2021-04-15 19:49:31 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,13 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      
+          <h1>Actors Page</h1>
+          {actors.map(actor =>     <div key={actor.name}>
+            <p>{actor.name}</p>
+            <ul>{actor.movies.map(movie=><li key={movie}>{movie}</li>)}</ul>
+          </div>)}
+        
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const displayMovie = (movie) => {
+
+  }
   return (
     <div>
-        {/*{code here}*/}
+        
+          <h1>Movies Page</h1>
+          {movies.map(movie =>     <div key={movie.title}>
+            <p>{movie.title}</p>
+            <p>{movie.time}</p>
+            <ul>{movie.genres.map(genre=><li key={genre}>{genre}</li>)}</ul>
+          </div>)}
+        
     </div>
   );
 };
+
 
 export default Movies;
