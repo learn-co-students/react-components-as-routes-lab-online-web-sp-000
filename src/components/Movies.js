882ecc4278d169877de5ad059a68ff6ad2d319a5
catprice444 <catprice444@gmail.com>
2021-05-10 16:55:35 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,29 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        {
+          <h1>Movies Page</h1>
+        }
+        {
+          movies.map(movie=>{
+            return(
+              <div>
+                <h2>Title: {movie.title}</h2>
+                <h3>Time: {movie.time}</h3>
+                <h3>Genres:</h3>
+                <ul>
+                  {movie.genres.map(genre=> <li>{genre}</li>)}
+                </ul>
+              </div>
+            )
+          })
+        }
     </div>
   );
 };
 
 export default Movies;
+
+// This component should render the text `Movies Page` in an `<h1>`, and make a new `<div>` for
+// each movie. The `<div>` should contain the movie's title, time and a `<ul>` with a list of its
+// genres.
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,10 +3,30 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      {
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/'>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to='/movies'>Movies</NavLink>
+            </li>
+            <li>
+              <NavLink to='/directors'>Directors</NavLink>
+            </li>
+            <li>
+              <NavLink to='/actors'>Actors</NavLink>
+            </li>
+          </ul>
+        </nav>
+    }
     </div>
   );
 };
 
 export default NavBar;
+
+// This component needs to render 4 `<NavLink>` components. They will be for __/,
+// /movies, /directors, /actors__ <-- in this order(test checks for this).
