c1945671f4bcc603ca6b9ffecf014acb2472dce1
clone18476 <cameroncousins18@gmail.com>
2021-05-22 14:44:59 -0500

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   BrowserRouter as Router,
+  Switch,
   Route
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
@@ -12,10 +13,38 @@ import Movies from '../components/Movies';
 
 const App = (props) => {
   return (
-    <Router>
-      {/*{code here}*/}
+    <Router> 
+      {
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/movies">
+              <Movies />
+            </Route>
+            <Route exact path="/directors">
+              <Directors />
+            </Route>
+            <Route exact path="/actors">
+              <Actors />
+            </Route>
+            </Switch>
+        </div>
+      }
     </Router>
   );
 };
 
 export default App
+
+// This component already contains a `Router` wrapper where we'll include our
+// particular routes. Inside this element, we'll need to render our `Navbar` and 4
+// __React Router__ `Route` components with paths to __/, /movies, /directors &
+// /actors__ and rendering the respective components. When a user visits the root
+// URL, they should see the `Home` component.
+
+// **Note**: Since a `Router` wrapper can only wrap one child element, use a `div` to
+// wrap the `Navbar` and `Route`s. This allows us to apply a CSS class at the App
+// component level.
