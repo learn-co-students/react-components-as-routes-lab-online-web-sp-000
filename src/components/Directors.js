32468b9e2a595b6504483d256b84711e3cf1bfe1
TrueC-D <aletheacary@gmail.com>
2021-06-16 18:16:06 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map((direct, index) => (
+        <div key={index}> 
+          <h3>{direct.name}</h3>
+          <label>Movies:</label>
+          <ul>
+            {direct.movies.map( (movie, key) =>
+            <li key={key}>{movie}</li>
+            )}
+          </ul>
+        </div>
+
+      ))}
+
+      
     </div>
   );
 }
