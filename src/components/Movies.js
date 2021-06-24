e45337fb95ee6e8cc1339ebae7f8c887f0af3e8a
jrog20 <jaimerogstad@gmail.com>
2021-06-24 12:40:37 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -7,7 +7,9 @@ const Movies = () => {
       <h1>Movies Page</h1>
       {movies.map((movie, index) => (
         <div key={index}>
-          <h2>{movie.title} - {movie.time} Minutes</h2>
+          <h2>Name: {movie.title}</h2>
+          <h3>Time: {movie.time}</h3>
+          <h3>Genres: </h3>
           <ul>{
             movie.genres.map((genre, index) => (
             <li key={index}>{genre}</li>
