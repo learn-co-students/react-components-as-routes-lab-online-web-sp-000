47ad92f5b444a2b01728cde2b85984f14a56e994
jrog20 <jaimerogstad@gmail.com>
2021-06-24 08:49:36 -0400

    Update movie page

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,11 +3,18 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
+    <div className="movies">
       <h1>Movies Page</h1>
-      <div>
-        {movies.map}
-      </div>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h2>{movie.title} - {movie.time} Minutes</h2>
+          <ul>{
+            movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
